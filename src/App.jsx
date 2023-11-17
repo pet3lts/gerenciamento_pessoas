2e3af727dad0a5170cf99ab9f502fd323bfd0803
David Rodrigues <regis_2725@hotmail.com>
2023-11-17 16:30:32 -0300

    incluindo config deploy

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Testando github pages!</h1>
+      <img src="https://loremflickr.com/640/360"/>
+    </>
+  )
+}
+
+export default App
